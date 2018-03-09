a2d0c7cd867178479f2996b952eb8d5287b7409b
Matthew J. H. Rattigan <mattratt@gmail.com>
2018-03-09 07:52:21 -0500

    aggreg answer cols

--- parseStackOvafloIII.py
+++ parseStackOvafloIII.py
@@ -220,7 +220,7 @@ if __name__ == '__main__':
         # add answer aggregs to questions, then join user table
         logging.info("aggreg answer cols")
         aggs = {'Score': 'mean', 'CommentCount': 'mean', 'Length': 'mean'}
-        answer_aggregs_df = ans_df.groupby('ParentId').agg()
+        answer_aggregs_df = ans_df.groupby('ParentId').agg(aggs)
         # answer_aggregs_df.columns = ["_".join(x) for x in answer_aggregs_df.columns.ravel()]
         # answer_aggregs_df.columns = []
         print answer_aggregs_df.head(), "\n", answer_aggregs_df.dtypes
